6a120d6863b042540bbdab962d0b8f2778752a3a
connor-maker <connor.hull.2011@gmail.com>
2024-12-03 19:09:14 -0800

    Create RoadrunnerAutohighbin.java
    checkin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadrunnerAutohighbin.java
@@ -0,0 +1,183 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.TankDrive;
+import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
+
+
+@Autonomous(name = "RoadrunnerAuto")
+public class RoadrunnerAutohighbin extends LinearOpMode {
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private DcMotor lift;
+    private DcMotor armTurn;
+    private CRServo intake;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public class LiftUp implements Action {
+        // checks if the lift motor has been powered on
+        private boolean initialized = false;
+
+        // actions are formatted via telemetry packets as below
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            // powers on motor, if it is not on
+            if (!initialized) {
+                lift.setPower(0.8);
+                initialized = true;
+            }
+
+            // checks lift's current position
+            double pos = lift.getCurrentPosition();
+            packet.put("liftPos", pos);
+            if (pos < -1850) {
+                // true causes the action to rerun
+                return true;
+            } else {
+                // false stops action rerun
+                lift.setPower(0);
+                return false;
+            }
+        }   // overall, the action powers the lift until it surpasses
+    }
+    public Action liftUp () {
+        return new LiftUp();
+    }
+    public class LiftDown implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!initialized) {
+                lift.setPower(-0.8);
+                initialized = true;
+            }
+
+            double pos = lift.getCurrentPosition();
+            packet.put("liftPos", pos);
+            if (pos > 0) {
+                return true;
+            } else {
+                lift.setPower(0);
+                return false;
+            }
+        }
+    }
+    public Action liftDown () {
+        return new LiftDown();
+    }
+    public class Outtake implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            intake.setPower(0.5);
+            return false;
+        }
+    }
+    public Action outtake () {
+        return new Outtake();
+    }
+    public class Intake implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            intake.setPower(-1.0);
+            return false;
+        }
+    }
+    public Action intake () {
+        return new Intake();
+    }
+    public class StopIntake implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            intake.setPower(0);
+            return false;
+        }
+    }
+    public Action stopIntake () {
+        return new StopIntake();
+    }
+    private void strafe() {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+                            .lineToY(-22)
+                            .build());
+        }
+
+    }
+
+    private void moveAbit() {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+                            .lineToX(4)
+                            .build());
+        }
+    }
+
+    @Override
+    public void runOpMode () {
+        LiftUp liftUp = new LiftUp();
+        LiftDown liftDown = new LiftDown();
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setTargetPosition(0);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armTurn = hardwareMap.get(DcMotor.class, "armTurn");
+        armTurn.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armTurn.setTargetPosition(0);
+        armTurn.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armTurn.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake = hardwareMap.get(CRServo.class, "intake");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        runtime.reset();
+
+
+
+
+
+
+        Actions.runBlocking(
+                new SequentialAction(
+
+                        liftUp(),
+                        liftDown()
+
+                )
+        );
+    }
+}
+
+
+
