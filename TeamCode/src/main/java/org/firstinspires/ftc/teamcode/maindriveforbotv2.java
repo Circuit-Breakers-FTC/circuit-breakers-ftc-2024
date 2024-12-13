820bfb1afae43a1e370735d88915baafae0b8102
connor-maker <connor.hull.2011@gmail.com>
2024-12-12 17:33:41 -0800

    Changed Syntax. This most likely won't be needed after this update.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/maindriveforbotv2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/maindriveforbotv2.java
@@ -15,10 +15,18 @@ public class maindriveforbotv2 extends LinearOpMode {
 
 
     private DcMotor armRotate;
-    ;
+
+    //Set the motors
+
     private Servo openCloseClaw;
     private Servo rotateClaw;
     private Servo lowerClaw;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    //Changable Variables
 
     public static double CLAW_UP = 0.85;
     public static double CLAW_DOWN = 0.6;
@@ -28,45 +36,117 @@ public class maindriveforbotv2 extends LinearOpMode {
     public static double CLAW_TWO = 0.325;
     public static double CLAW_THREE = 0.15;
     public static double CLAW_FOUR = 0.675;
-    /**
-     * This function is executed when this Op Mode is selected from the Driver Station.
-     */
+
+    //Function is Executed when OpMode is initiated
+
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
+
+        //Set Drive Motors
+
+        frontLeft = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight = hardwareMap.get(DcMotor.class, "backLeft");
+        backLeft = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        //Set Sample Arm Motors
+
         openCloseClaw = hardwareMap.get(Servo.class, "openCloseClaw");
         rotateClaw = hardwareMap.get(Servo.class, "rotateClaw");
         lowerClaw = hardwareMap.get(Servo.class, "lowerClaw");
         armRotate = hardwareMap.get(DcMotor.class, "armRotate");
-        waitForStart();
+
+        waitForStart(); //Wait for Opmode Activation
+
         while (opModeIsActive()) {
+
             telemetry.addData("lower.position", lowerClaw.getPosition());
             telemetry.update();
-            if (gamepad2.y) {
-                lowerClaw.setPosition(CLAW_UP);
-            }
-            if (gamepad2.a) {
-                lowerClaw.setPosition(CLAW_DOWN);
-            }
-            if (gamepad2.x) {
-                openCloseClaw.setPosition(CLAW_OPEN);
-            }
-            if (gamepad2.b) {
-                openCloseClaw.setPosition(CLAW_CLOSE);
-            }
-            if (gamepad2.dpad_up) {
-                rotateClaw.setPosition(CLAW_ONE);
-            }
-            if (gamepad2.dpad_right) {
-                rotateClaw.setPosition(CLAW_TWO);
-            }
-            if (gamepad2.dpad_down) {
-                rotateClaw.setPosition(CLAW_THREE);
-            }
-            if (gamepad2.dpad_left) {
-                rotateClaw.setPosition(CLAW_FOUR);
-            }
 
+            //Main Driving Options
+                boolean fastMode = false;
+                if (gamepad1.right_bumper) {
+                    fastMode = true;
+                } else {
+                    fastMode = false;
+                  telemetry.addLine("right bumper for fast mode!");
+                }
+
+                if (gamepad2.y) {
+                    lowerClaw.setPosition(CLAW_UP);
+                }
+
+                if (gamepad2.a) {
+                    lowerClaw.setPosition(CLAW_DOWN);
+                }
+
+                if (gamepad2.x) {
+                    openCloseClaw.setPosition(CLAW_OPEN);
+                }
+
+                if (gamepad2.b) {
+                    openCloseClaw.setPosition(CLAW_CLOSE);
+                }
+
+                if (gamepad2.dpad_up) {
+                    rotateClaw.setPosition(CLAW_ONE);
+                }
+
+                if (gamepad2.dpad_right) {
+                    rotateClaw.setPosition(CLAW_TWO);
+                }
+
+                if (gamepad2.dpad_down) {
+                    rotateClaw.setPosition(CLAW_THREE);
+                }
+
+                if (gamepad2.dpad_left) {
+                    rotateClaw.setPosition(CLAW_FOUR);
+
+
+                    //Driving Options
+
+                    double x = gamepad1.left_stick_x; // Strafe left/right
+                    double y = -gamepad1.left_stick_y; // Forward/backward
+                    double rotation = gamepad1.right_stick_x; // Rotate
+
+                    // Calculate motor powers
+
+                    double frontLeftPower = y + x + rotation;
+                    double frontRightPower = y - x - rotation;
+                    double backLeftPower = y - x + rotation;
+                    double backRightPower = y + x - rotation;
+                    double slowness = (.5);
+
+                    //Set Motor Power Limits to 1
+
+                    double maxPower = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower),
+                            Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
+
+                    if (maxPower > 1.0) {
+                        frontLeftPower /= maxPower;
+                        frontRightPower /= maxPower;
+                        backLeftPower /= maxPower;
+                        backRightPower /= maxPower;
+                    }
+
+                    if (fastMode) {
+                        slowness = 1.0;
+                    }
+
+                    // Set motor powers
+
+                    frontLeft.setPower(frontLeftPower * slowness);
+                    frontRight.setPower(frontRightPower * slowness);
+                    backLeft.setPower(backLeftPower * slowness);
+                    backRight.setPower(backRightPower * slowness);
+
+                }
+            }
         }
 
     }
-}
