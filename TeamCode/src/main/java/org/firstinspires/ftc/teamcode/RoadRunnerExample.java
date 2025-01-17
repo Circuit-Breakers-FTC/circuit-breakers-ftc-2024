a218aeb9621ccc494e67a151ea64d98f8ac5ac50
rory-maker <puddle.jumper.rory@gmail.com>
2025-01-16 23:23:17 -0800

    first version of 3 sample autonomy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerExample.java
@@ -12,12 +12,13 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "RoadRunnerExample")
+@Autonomous(name = "RoadRunnerExample")
 @Config
 public final class RoadRunnerExample extends LinearOpMode {
     private DcMotor lift;
@@ -26,9 +27,25 @@ public final class RoadRunnerExample extends LinearOpMode {
 
 
     public static double DEPOSIT_POSITION_X = 22;
-    public static double DEPOSIT_POSITION_Y = 5*24+4;
+    public static double DEPOSIT_POSITION_Y = 126.5;
     public static double DEPOSIT_POSITION_HEADING = 135;
     public static double DEPOSIT_POSITION_DIRECTION = 75;
+    public static double PICKUP_POSITION_X = 48;
+    public static double PICKUP_POSITION_Y = 105;
+    public static double PICKUP_POSITION_HEADING = 90;
+    public static double PICKUP_POSITION_DIRECTION = 90;
+    public static double PICKUP_POSITION_Y2 = 115;
+    public static double PUSH_POSITION_END_X = 50;
+    public static double PUSH_POSITION_END_Y = 115;
+    public static double PUSH_POSITION_HEADING = 80;
+    public static double PUSH_POSITION_DIRECTION = 180;
+    public static double PICKUP_POSITION_Y3 = 130;
+    public static int ARM_PICKUP = -2720;
+    public static int ARM_BIN =-1490;
+    public static double ARM_POWER = 1.0;
+    public static double LIFT_POWER = 1.0;
+    public static double DEPOSIT_SLEEP_TIME = 1.5;
+
     public void runOpMode() throws InterruptedException {
         lift = hardwareMap.get(DcMotor.class, "lift");
         lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -45,6 +62,10 @@ public final class RoadRunnerExample extends LinearOpMode {
 
         Pose2d beginPose = new Pose2d(8, 4*24+8.5, Math.toRadians(90));
         Pose2d depositPose = new Pose2d(DEPOSIT_POSITION_X, DEPOSIT_POSITION_Y, Math.toRadians(DEPOSIT_POSITION_HEADING));
+        Pose2d pickupPose = new Pose2d(PICKUP_POSITION_X,PICKUP_POSITION_Y,Math.toRadians((PICKUP_POSITION_HEADING)));
+        Pose2d pickupPose2 = new Pose2d(PICKUP_POSITION_X,PICKUP_POSITION_Y2,Math.toRadians((PICKUP_POSITION_HEADING)));
+        Pose2d pushPose = new Pose2d(PUSH_POSITION_END_X,PUSH_POSITION_END_Y,Math.toRadians((PUSH_POSITION_HEADING)));
+        Pose2d pickupPose3 = new Pose2d(PICKUP_POSITION_X,PICKUP_POSITION_Y3,Math.toRadians((PICKUP_POSITION_HEADING)));
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
         waitForStart();
         telemetry.addLine("Starting");
@@ -52,17 +73,73 @@ public final class RoadRunnerExample extends LinearOpMode {
         Actions.runBlocking(
             new SequentialAction(
                     new ParallelAction(
-                            new MotorAction(lift, -1700, 1),
-                            new MotorAction(armTurn, -1490, 0.5),
+                            new MotorAction(armTurn,ARM_BIN, ARM_POWER),
+                            new MotorAction(lift, -1700, LIFT_POWER),
                             drive.actionBuilder(beginPose)
                                     .setTangent(0)
                                     .splineToLinearHeading(depositPose, Math.toRadians(DEPOSIT_POSITION_DIRECTION))
                                     .build()
                     ),
-                    new CRServoAction(intake, 0.25),
+                    new CRServoAction(intake, 0.35),
+                    new SleepAction(DEPOSIT_SLEEP_TIME),
+                    drive.actionBuilder(depositPose)
+                            .setTangent(-90)
+                            .splineToLinearHeading(pushPose, Math.toRadians(PUSH_POSITION_DIRECTION))
+                            .build(),
+                    new ParallelAction(
+                           new MotorAction(armTurn, -2510, 0.5),
+                           new MotorAction(lift, 0, 0.30),
+                            drive.actionBuilder(pushPose)
+                                    .setTangent(-135)
+                                    .splineToLinearHeading(pickupPose, Math.toRadians(PICKUP_POSITION_DIRECTION))
+                                    .build()
+                    ),
+                    new CRServoAction(intake, -1),
+                    new MotorAction(armTurn, ARM_PICKUP, 0.5),
+                    new SleepAction(1),
+                    drive.actionBuilder(pickupPose)
+                        .setTangent(90)
+                        .splineToLinearHeading(pickupPose2, Math.toRadians(PICKUP_POSITION_DIRECTION))
+                        .build(),
+                    new SleepAction(0.5),
+                    new ParallelAction(
+                            new MotorAction(armTurn,ARM_BIN, 0.5),
+                            new MotorAction(lift, -1700, 0.5),
+                            drive.actionBuilder(pickupPose2)
+                                    .setTangent(180)
+                                    .splineToLinearHeading(depositPose, Math.toRadians(DEPOSIT_POSITION_DIRECTION))
+                                    .build()
+                    ),
+                    new CRServoAction(intake, 0.35),
+                    new SleepAction(2),
+                    new ParallelAction(
+                            new MotorAction(armTurn, -2510, 0.5),
+                            new MotorAction(lift, 0, 0.30),
+                            drive.actionBuilder(depositPose)
+                                    .setTangent(-30)
+                                    .splineToLinearHeading(pickupPose, Math.toRadians(PICKUP_POSITION_DIRECTION))
+                                    .build()
+                    ),
+                    new CRServoAction(intake, -1),
+                    new MotorAction(armTurn, ARM_PICKUP, 0.5),
+                    drive.actionBuilder(pickupPose)
+                            .setTangent(90)
+                            .splineToLinearHeading(pickupPose3, Math.toRadians(PICKUP_POSITION_DIRECTION))
+                            .build(),
+                    new SleepAction(1),
+                    new ParallelAction(
+                        new MotorAction(armTurn,ARM_BIN, 0.5),
+                        new MotorAction(lift, -1700, 0.5),
+                        drive.actionBuilder(pickupPose2)
+                           .setTangent(180)
+                          .splineToLinearHeading(depositPose, Math.toRadians(DEPOSIT_POSITION_DIRECTION))
+                           .build()
+                    ),
+                    new CRServoAction(intake, 0.35),
                     new SleepAction(2)
-                )
+            )
         );
+
         telemetry.addLine("Finished");
         telemetry.update();
     }
